d88db6ccff9192441f28b8028a951ff6bacaa134
제주 <qwert6603@naver.com>
2019-06-28 05:31:09 +0900

    구초제 학습

--- Yun_C/Chapter22/structure.c
+++ Yun_C/Chapter22/structure.c
@@ -170,29 +170,139 @@
 
 //practice 2
 
-struct employee
+//struct employee
+//{
+//	char name[20];
+//	char personal[20];
+//	int pay;
+//};
+//
+//int main(void)
+//{
+//	struct employee arr[3];
+//	
+//	for (int i = 0; i < 3; i++)
+//	{
+//		printf("이름 입력 : ");
+//		scanf("%s", arr[i].name);
+//		printf("주민번호 입력 : ");
+//		scanf("%s", arr[i].personal);
+//		printf("급여정보 입력 : ");
+//		scanf("%d", &arr[i].pay);
+//	}
+//
+//	for (int i = 0; i < 3; i++)
+//		printf("이름 : %s \n주민번호 : %s \n급여정보 : %d \n", arr[i].name, arr[i].personal, arr[i].pay);
+//
+//	return 0;
+//}
+
+
+//구조체 포인터
+
+//struct point
+//{
+//	int xpos;
+//	int ypos;
+//};
+//
+//int main(void)
+//{
+//	struct point pos1 = { 1, 2 };
+//	struct point pos2 = { 100, 200 };
+//	struct point* p = &pos1;
+//
+//	(*p).xpos += 4;
+//	(*p).ypos += 5;
+//	printf("[%d %d] \n", p->xpos, p->ypos);
+//
+//	p = &pos2;
+//	p->xpos += 1;
+//	p->ypos += 2;
+//	printf("[%d %d] \n", (*p).xpos, (*p).ypos);
+//
+//	return 0;
+//}
+
+
+//포인터 변수를 구조체 맴버로 사용
+
+//struct point
+//{
+//	int xpos;
+//	int ypos;
+//};
+//
+//struct circle
+//{
+//	double radius;
+//	struct point * center;
+//
+//};
+//
+//int main(void)
+//{
+//	struct point cen = { 2, 7 };
+//	double rad = 5.5;
+//
+//	struct circle ring = { rad, &cen };
+//	printf("원의 반지름 : %f \n", ring.radius);
+//	printf("원의 중심 [%d %d] \n", (ring.center)->xpos, (ring.center)->ypos);
+//
+//	return 0;
+//}
+
+
+//구조체 변수맴버로 자체 포인터변수 사용
+
+//struct point
+//{
+//	int xpos;
+//	int ypos;
+//	struct point* p;
+//};
+//
+//int main(void)
+//{
+//	struct point pos1 = { 1, 1 };
+//	struct point pos2 = { 2, 2 };
+//	struct point pos3 = { 3, 3 };
+//
+//	pos1.p = &pos2;
+//	pos2.p = &pos3;
+//	pos3.p = &pos1;
+//
+//	printf("----점의 연결관계----  \n");
+//	printf("[%d %d] and [%d %d] 연결 \n", pos1.xpos, pos1.ypos, pos1.p->xpos, pos1.p->ypos);
+//	printf("[%d %d] and [%d %d] 연결 \n", pos2.xpos, pos2.ypos, pos2.p->xpos, pos2.p->ypos);
+//	printf("[%d %d] and [%d %d] 연결 \n", pos3.xpos, pos3.ypos, pos3.p->xpos, pos3.p->ypos);
+//
+//	return 0;
+//}
+
+
+//구조체 변수의 주소값
+
+struct point
+{
+	int xpos;
+	int ypos;
+};
+
+struct person
 {
 	char name[20];
-	char personal[20];
-	int pay;
+	char phoneNum[20];
+	int age;
 };
 
 int main(void)
 {
-	struct employee arr[3];
-	
-	for (int i = 0; i < 3; i++)
-	{
-		printf("이름 입력 : ");
-		scanf("%s", arr[i].name);
-		printf("주민번호 입력 : ");
-		scanf("%s", arr[i].personal);
-		printf("급여정보 입력 : ");
-		scanf("%d", &arr[i].pay);
-	}
+	struct point pos = { 10, 20 };
+	struct person man = { "이승기", "010-1234-5678", 21 };
 
-	for (int i = 0; i < 3; i++)
-		printf("이름 : %s \n주민번호 : %s \n급여정보 : %d \n", arr[i].name, arr[i].personal, arr[i].pay);
+	printf("%p %p \n", &pos, &pos.xpos);
+	printf("%p %p \n", &man, &man.name);
 
 	return 0;
 }
