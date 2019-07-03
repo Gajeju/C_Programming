bde5e2401de384419d6f58411a4971bb38858fc4
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-07-04 07:33:36 +0900

    구조체 학습

--- Yun_C/Chapter22/structure.c
+++ Yun_C/Chapter22/structure.c
@@ -520,32 +520,72 @@
 
 //practice
 
-typedef struct point
-{
-	int xpos;
-	int ypos;
-} Point;
+//typedef struct point
+//{
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//void Swap(Point * p1, Point * p2)
+//{
+//	Point temp;
+//
+//	temp = *p1;
+//	*p1 = *p2;
+//	*p2 = temp;
+//}
+//
+//int main(void)
+//{
+//	Point pos1 = { 2, 4 };
+//	Point pos2 = { 5, 7 };
+//
+//	Swap(&pos1, &pos2);
+//
+//	printf("[%d %d] \n", pos1.xpos, pos1.ypos);
+//	printf("[%d %d] \n", pos2.xpos, pos2.ypos);
+//
+//	return 0;
+//}
 
-void Swap(Point * p1, Point * p2)
-{
-	Point temp;
 
-	temp = *p1;
-	*p1 = *p2;
-	*p2 = temp;
+//구조체를 사용하는 이유
+
+typedef struct studnet
+{
+	char name[20];
+	char stdnum[20];
+	char school[20];
+	char major[20];
+	int year;
+} Student;
+
+void ShowStudentInfo(Student* sptr)
+{
+	printf("학생 이름 : %s \n", sptr->name);
+	printf("학생 고유번호 : %s \n", sptr->stdnum);
+	printf("학교 이름 : %s \n", sptr->school);
+	printf("선택 정공 : %s \n", sptr->major);
+	printf("학년 : %d \n", sptr->year);
 }
 
 int main(void)
 {
-	Point pos1 = { 2, 4 };
-	Point pos2 = { 5, 7 };
+	Student arr[7];
+	
+	for (int i = 0; i < 7; i++)
+	{
+		printf("이름 : "); scanf("%s", arr[i].name);
+		printf("번호 : "); scanf("%s", arr[i].stdnum);
+		printf("학교 : "); scanf("%s", arr[i].school);
+		printf("전공 : "); scanf("%s", arr[i].major);
+		printf("학년 : "); scanf("%d", &arr[i].year);
+	}
 
-	Swap(&pos1, &pos2);
-
-	printf("[%d %d] \n", pos1.xpos, pos1.ypos);
-	printf("[%d %d] \n", pos2.xpos, pos2.ypos);
+	for (int i = 0; i < 7; i++)
+	{
+		ShowStudentInfo(&arr[i]);
+	}
 
 	return 0;
 }
-
-
