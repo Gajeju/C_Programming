b54bb5baf4942f4494d988b838a80dec01278d0f
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-07-06 10:04:00 +0900

    구조체및 공동체

--- Yun_C/Chapter22/structure.c
+++ Yun_C/Chapter22/structure.c
@@ -621,3 +621,72 @@
 //	return 0;
 //}
 
+
+//practice2
+
+//typedef struct point
+//{
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//typedef struct rectangle
+//{
+//	Point lr;
+//	Point ul;
+//
+//} Rectangle;
+//
+//void ShowRecArea(Rectangle rec)
+//{
+//	printf("넓이 : %d \n", (rec.lr.xpos - rec.ul.xpos) * (rec.lr.ypos - rec.ul.ypos));
+//}
+//
+//void ShowRecPos(Rectangle rec)
+//{
+//	printf("좌 상단 : [%d %d] \n", rec.ul.xpos, rec.ul.ypos);
+//	printf("좌 하단 : [%d %d] \n", rec.ul.xpos, rec.lr.ypos);
+//	printf("우 상단 : [%d %d] \n", rec.lr.xpos, rec.ul.ypos);
+//	printf("우 하단 : [%d %d] \n", rec.lr.xpos, rec.lr.ypos);
+//}
+//
+//int main(void)
+//{
+//	Rectangle rec1 = { {1,1}, {4,4} };
+//	Rectangle rec2 = { {0,0}, {7,5} };
+//	ShowRecArea(rec1);
+//	ShowRecPos(rec1);
+//	ShowRecArea(rec2);
+//	ShowRecPos(rec2);
+//	
+//	return 0;
+//}
+
+
+//공동체
+
+typedef struct sbos
+{
+	int men1;
+	int men2;
+	double men3;
+} SBox;
+
+typedef union ubox
+{
+	int men1;
+	int men2;
+	double men3;
+} UBox;
+
+int main(void)
+{
+	SBox sbx;
+	UBox ubx;
+	printf("%p %p %p \n", &sbx.men1, &sbx.men2, &sbx.men3);
+	printf("%p %p %p \n", &ubx.men1, &ubx.men2, &ubx.men3);
+	printf("%d %d \n", sizeof(SBox), sizeof(UBox));
+
+	return 0;
+}
+
