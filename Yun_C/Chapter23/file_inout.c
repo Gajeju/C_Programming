8dbbf6f685d41f25a317208c14e0bf14e6bec053
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-07-10 08:49:00 +0900

    파일 입출력 학습

--- Yun_C/Chapter23/file_inout.c
+++ Yun_C/Chapter23/file_inout.c
@@ -96,13 +96,41 @@
 //}
 
 
-//파일 복사
+//feof 기반 파일 복사
+
+//int main(void)
+//{
+//	FILE* src = fopen("src.txt", "rt");
+//	FILE* des = fopen("des.txt", "wt");
+//	int ch;
+//
+//	if (src == NULL || des == NULL)
+//	{
+//		puts("파일오픈 실패");
+//		return -1;
+//	}
+//
+//	while ((ch = fgetc(src)) != EOF)
+//		fputc(ch, des);
+//	if (feof(src) != 0)
+//		puts("파일복사 완료");
+//	else
+//		puts("파일복사 실패");
+//
+//	fclose(src);
+//	fclose(des);
+//
+//	return 0;
+//}
+
+
+//문자열 단위 파일 복사
 
 int main(void)
 {
 	FILE* src = fopen("src.txt", "rt");
 	FILE* des = fopen("des.txt", "wt");
-	int ch;
+	char str[20];
 
 	if (src == NULL || des == NULL)
 	{
@@ -110,8 +138,8 @@ int main(void)
 		return -1;
 	}
 
-	while ((ch = fgetc(src)) != EOF)
-		fputc(ch, des);
+	while (fgets(str, sizeof(str), src) != NULL)
+		fputs("str, des");
 	if (feof(src) != 0)
 		puts("파일복사 완료");
 	else
